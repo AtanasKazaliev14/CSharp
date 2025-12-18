a907a5303130b5525a5599eebb8cd04ba254312a
AtanasKazaliev14 <akkazaliev24a@gmail.com>
2025-12-18 10:26:20 +0200

    Add Main method to check if a number is even or odd

--- app.cs
+++ app.cs
@@ -1,1 +1,16 @@
-
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else 
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
